23b41a5d4a683a79bf580c06718a737bbd9cc7d5
Ziad Khaled <ziadkhaled1419@gmail.com>
2018-08-18 00:57:10 +0200

    changed the basic structure of project because, I started to use an IDE instead of a text editor

---
+++ src/nubahome/databse/Database.java
@@ -0,0 +1,5 @@
+package nubahome.databse;
+
+public class Database {
+
+}
---
+++ src/nubahome/login/Login.java
@@ -0,0 +1,11 @@
+package nubahome.login;
+
+public class Login {
+
+    public static final int clerkPriviliges = 1;
+    public static final int managerPriviliges = 2;
+
+    public void login(String userName, String userPassword) {
+
+    }
+}
